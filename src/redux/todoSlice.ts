e0c09785ae2f49c33505e793c164bd38b0974281
Oleksandra Makar <oleksandra.makar@dataart.com>
2022-12-23 20:15:19 +0100

    removed redundant code

--- src/redux/todoSlice.ts
+++ src/redux/todoSlice.ts
@@ -4,7 +4,7 @@ import { IToDo } from '../interfaces/Todo'
 const initialState: IToDo[] = [
     { id: 1, title: 'todo1', completed: false },
     { id: 2, title: 'todo2', completed: false },
-    { id: 3, title: 'todo3', completed: true },
+    { id: 3, title: 'todo3', completed: false },
     { id: 4, title: 'todo4', completed: false },
 ]
 
